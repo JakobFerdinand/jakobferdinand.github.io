7bef831446ab8921e77c3967154e6dacaf639016
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2019-12-14 10:06:00 +0100

    add elm-live-server script

---
+++ src/elm-live-server.sh
@@ -0,0 +1,5 @@
+# if elm-live is missing run:
+# npm install --global elm elm-live
+# https://github.com/wking-io/elm-live
+
+elm-live src/Main.elm --start-page=index.html -- --output=elm.js --debug
