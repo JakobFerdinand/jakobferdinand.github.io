d518e3352f2d942518a23ceb6869c350ed4e3b53
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2019-12-18 21:52:46 +0100

    Display Spaces

--- src/src/PaletteView.elm
+++ src/src/PaletteView.elm
@@ -1,10 +1,11 @@
 module PaletteView exposing (Model, Msg, init, update, view)
 
 import Browser
-import Element exposing (Color, Element, centerX, centerY, column, el, fill, height, html, row, spacing, text)
+import Element exposing (Color, Element, alignBottom, alignLeft, centerX, centerY, column, el, fill, height, html, maximum, minimum, row, spacing, text, width)
+import Element.Font as Font
 import Palette exposing (..)
-import Svg exposing (circle, svg)
-import Svg.Attributes
+import Svg exposing (circle, rect, svg)
+import Svg.Attributes as Attributes
 
 
 type alias Model =
@@ -29,16 +30,29 @@ update msg model =
 
 view : Model -> Element Msg
 view model =
-    row
-        [ spacing large
-        , centerX
-        , centerY
-        ]
-        [ viewColor { name = "foreground", color = foregroundColor, colorString = foregroundColorString }
-        , viewColor { name = "background", color = backgroundColor, colorString = backgroundColorString }
-        , viewColor { name = "accent1", color = accent1Color, colorString = accent1ColorString }
-        , viewColor { name = "accent2", color = accent2Color, colorString = accent2ColorString }
-        , viewColor { name = "accent3", color = accent3Color, colorString = accent3ColorString }
+    column [ spacing normal ]
+        [ el [ alignLeft, Font.heavy ] (text "Colors")
+        , row
+            [ spacing large
+            , centerX
+            , centerY
+            ]
+            [ viewColor { name = "foreground", color = foregroundColor, colorString = foregroundColorString }
+            , viewColor { name = "background", color = backgroundColor, colorString = backgroundColorString }
+            , viewColor { name = "accent1", color = accent1Color, colorString = accent1ColorString }
+            , viewColor { name = "accent2", color = accent2Color, colorString = accent2ColorString }
+            , viewColor { name = "accent3", color = accent3Color, colorString = accent3ColorString }
+            ]
+        , el [ Font.heavy ] (text "Spacings")
+        , row
+            [ spacing large ]
+            [ viewSpacing "xxSmall" xxSmall
+            , viewSpacing "xSmall" xSmall
+            , viewSpacing "small" small
+            , viewSpacing "normal" normal
+            , viewSpacing "large" large
+            , viewSpacing "xLarge" xLarge
+            ]
         ]
 
 
@@ -54,19 +68,56 @@ viewColor color =
         ]
 
 
+viewSpacing : String -> Int -> Element Msg
+viewSpacing spacingName spacingValue =
+    let
+        header : String
+        header =
+            spacingName ++ (" " ++ String.fromInt spacingValue)
+    in
+    column [ width fill, alignBottom, spacing normal ]
+        [ el [ centerX ]
+            (html
+                (svg
+                    [ Attributes.height <| String.fromInt spacingValue
+                    , Attributes.width "20"
+                    ]
+                    [ rect
+                        [ Attributes.width "20"
+                        , Attributes.height <| String.fromInt spacingValue
+                        , Attributes.fill "orange"
+                        ]
+                        []
+                    ]
+                )
+            )
+        , text header
+        ]
+
+
 colorCircle : String -> Element Msg
 colorCircle color =
-    el []
+    let
+        fill100 =
+            fill
+                |> maximum 100
+                |> minimum 100
+    in
+    el
+        [ centerX
+        , height fill100
+        , width fill100
+        ]
         (html
             (svg
-                [ Svg.Attributes.width "100"
-                , Svg.Attributes.height "100"
+                [ Attributes.width "100"
+                , Attributes.height "100"
                 ]
                 [ circle
-                    [ Svg.Attributes.cx "50"
-                    , Svg.Attributes.cy "50"
-                    , Svg.Attributes.r "50"
-                    , Svg.Attributes.fill color
+                    [ Attributes.cx "50"
+                    , Attributes.cy "50"
+                    , Attributes.r "50"
+                    , Attributes.fill color
                     ]
                     []
                 ]
